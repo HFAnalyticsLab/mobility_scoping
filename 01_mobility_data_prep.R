36a56a1fd39e690455f84bedc567626faab208d9
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-03-31 13:48:54 +0000

    tidy up code

--- 01_mobility_data_prep.R
+++ 01_mobility_data_prep.R
@@ -38,25 +38,6 @@ pacman::p_load(haven,
                stringr, 
                here)
 
-
-list.of.packages.cran <- c(
-  "arm", "car", "corrplot", "FRK", "gghighlight",
-  "ggplot2", "ggmap", "GISTools", "gridExtra", "gstat",
-  "jtools", "kableExtra", "knitr", "lme4", "lmtest",
-  "lubridate", "MASS", "merTools", "plyr", "RColorBrewer",
-  "rgdal", "sf", "sjPlot", "sp", "spgwr",
-  "spatialreg", "spacetime", "stargazer", "tidyverse", "tmap",
-  "viridis", "tufte"
-)
-
-new.packages.cran <- list.of.packages.cran[!(list.of.packages.cran %in% installed.packages()[,"Package"])]
-if(length(new.packages.cran)) install.packages(new.packages.cran)
-
-for(i in 1:length(list.of.packages.cran)) {
-  library(list.of.packages.cran[i], character.only = T)
-}
-
-
 # set working directory (local/fixed pathway)
   #NOTE: obsolete since project sets wd and using "here"
   #setwd("M:/Analytics/Francesca/Mobility_data")
