d106e7e2fab492e8d6495ccabda40642feaecace
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-yzaz13.tier0.prd.thf.aws>
2022-05-19 08:59:20 +0000

    add CDRC data and tidy up

--- 08_CDRC_data_prep.R
+++ 08_CDRC_data_prep.R
@@ -1,17 +1,15 @@
 #save script in bucket 
 ## save this script to that folder in the bucket
-buck <- 'thf-dap-tier0-projects-iht-067208b7-projectbucket-1mrmynh0q7ljp/Francesca/mobility_scoping/' ## my bucket name
-
-put_object(
-  file = '08_CRDC_data_prep.R',
-  object = '08_CDRC_data_prep.R',
-  bucket = buck
-)
+# buck <- 'thf-dap-tier0-projects-iht-067208b7-projectbucket-1mrmynh0q7ljp/Francesca/mobility_scoping/' ## my bucket name
+# 
+# put_object(
+#   file = '08_CRDC_data_prep.R',
+#   object = '08_CDRC_data_prep.R',
+#   bucket = buck
+# )
 
 
 #Load LSOA shape file 
-
-# Option 2: trying command above with save_object to save directly into R workspace without manually downloading
 
 
 # Need to download all 6 files in folder for .shp to load correctly
@@ -34,6 +32,17 @@ lsoa_shp <- st_read(here::here("shapefiles", "eng.shp"))
 str(lsoa_shp)
 
 
+#Prep CDRC data (only have LSOA or LAD)
+
+buck <- 'thf-dap-tier0-projects-iht-067208b7-projectbucket-1mrmynh0q7ljp/Francesca/mobility_scoping/data' ## my bucket name
+
+
+cdrc_lsoa<-s3read_using(read_csv # Which function are we using to read
+                        , object = 'CDRC_residential_mobility_index_LSOA.csv' # File to open
+                        , bucket = buck) # Bucket name defined above
+
+#have churn of every year from 1997-2019 compared to 2020
+summary(cdrc_lsoa)
 
 
 
