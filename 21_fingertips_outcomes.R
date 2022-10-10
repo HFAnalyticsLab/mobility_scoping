80c153c1a713a2a1c41b04d510d35fc41bc0f275
Francesca Cavallaro (tier0) <tier0\francesca.cavallaro@ec2amaz-gjzwjg.tier0.prd.thf.aws>
2022-10-10 13:52:49 +0000

    Edited name of script - test

---
+++ 21_fingertips_outcomes.R
@@ -0,0 +1,13 @@
+## Download data from Fingertips
+
+
+# clear R environment
+rm(list = ls())
+
+#load packages
+pacman::p_load(plyr,
+               dplyr, 
+               fingertipsR)
+
+
+install.packages('fingertipsR')
